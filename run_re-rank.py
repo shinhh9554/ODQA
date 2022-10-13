e85bbf80a0bea050bf8b2391a591d1439f0933b2
ICL <2016ICL1618@gmail.com>
2022-10-13 22:45:53 +0900

    re-rank 모델 train file 추가

---
+++ run_re-rank.py
@@ -0,0 +1,16 @@
+import argparse
+
+from datasets import load_dataset
+
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+
+
+
+def main():
+	args = parse_args()
+
+
+if __name__ == '__main__':
+	main()
